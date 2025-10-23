08755c1a5503745af0ce95237b9bb30680e7aea0
marciodev <marcio@marciopaulo.com.br>
2025-10-23 20:18:35 -0300

    initial files

---
+++ time-sheet-ai-service/src/main/java/com/marciopaulo/timesheet/TimeSheetAiApplication.java
@@ -0,0 +1,13 @@
+package com.marciopaulo.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSheetAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetAiApplication.class, args);
+	}
+
+}
---
+++ time-sheet-ai-service/src/test/java/com/marciopaulo/timesheet/TimeSheetAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marciopaulo.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSheetAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
